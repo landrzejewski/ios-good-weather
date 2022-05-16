0d7c8814c88b0a76e6572e8b55d039f983203666
landrzejewski <landrzejewski.poczta@gmail..com>
2022-05-16 10:17:55 +0200

    Initial Commit

---
+++ GoodWeather/GoodWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodWeatherApp.swift
+//  GoodWeather
+//
+//  Created by Łukasz Andrzejewski on 16/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct GoodWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
